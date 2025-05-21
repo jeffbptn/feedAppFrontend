6b30aa4ff1b0b6a9869c3c0ceb5272c8160533e6
jeffbptn <jeff@bptn.com>
2025-05-21 11:29:41 -0400

    11.3 fixed

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./appRoutes/AppRoutes";
 import AppContextProvider from "./context/applicationContext";
+import { CookiesProvider } from "react-cookie";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AppContextProvider>
-    <Router>
-      <AppRoutes />
-    </Router>
-  </AppContextProvider>
+  <CookiesProvider>
+    <AppContextProvider>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </AppContextProvider>
+  </CookiesProvider>
 );
