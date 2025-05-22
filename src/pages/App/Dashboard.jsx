8a9ac333ce0f75c8743c378c9d1cd0c4f8f5369b
jeffbptn <jeff@bptn.com>
2025-05-22 09:43:13 -0400

    Coding 11.35 + cleanup

--- src/pages/App/Dashboard.jsx
+++ src/pages/App/Dashboard.jsx
@@ -1,12 +1,104 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
+
+import { AppContext } from "../../context/applicationContext";
+import LoadingIndicator from "../../components/LoadingIndicator";
+
+import { getOthersFeedsApi } from "../../util/ApiUtil";
+
 import MyProfile from "../../components/MyProfile";
 import AddFeed from "../../components/AddFeed";
+import InfiniteScroll from "react-infinite-scroll-component";
+import FeedCard from "../../components/FeedCard";
 
 const Dashboard = () => {
+  const appContext = useContext(AppContext);
+  const token = appContext.getSession();
+  const userData = appContext.getUserData();
+
+  const [feedsData, setFeedsData] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    document.title = "Home | Feed App";
+    getOthersFeeds(0);
+  }, []);
+
+  if (!userData) {
+    return <LoadingIndicator />;
+  }
+
+  const getOthersFeeds = async (loadPageNumber) => {
+    if (loadPageNumber === 0) {
+      setFeedsData([]);
+    }
+
+    const apiResponse = await getOthersFeedsApi(token, loadPageNumber);
+
+    console.log(apiResponse);
+
+    if (apiResponse.status === 1) {
+      let feedsDataNew = [];
+      if (loadPageNumber !== 0) {
+        feedsDataNew = feedsData;
+      }
+      feedsDataNew.push(...apiResponse.payLoad.content);
+      setFeedsData(feedsDataNew);
+
+      setPageNumber(loadPageNumber + 1);
+
+      if (loadPageNumber + 1 === apiResponse.payLoad.totalPages) {
+        setHasMore(false);
+      } else {
+        setHasMore(true);
+      }
+    }
+  };
+
   return (
-    <main>
+    <main className="grid grid-cols-1 lg:grid-cols-2 gap-6 my-12 mx-0 md:mx-12 w-2xl container px-2">
       <MyProfile />
-      <AddFeed />
+      <article>
+        <AddFeed />
+        <InfiniteScroll
+          dataLength={feedsData.length}
+          next={() => getOthersFeeds(pageNumber)}
+          hasMore={hasMore}
+          endMessage={
+            <p className="text-center">
+              <b>Yay! You have seen it all.</b>
+            </p>
+          }
+          refreshFunction={() => getOthersFeeds(0)}
+          pullDownToRefresh
+          pullDownToRefreshThreshold={50}
+          pullDownToRefreshContent={
+            <h3 className="text-center">&#8595; Pull down to refresh</h3>
+          }
+          releaseToRefreshContent={
+            <h3 className="text-center">&#8593; Release to refresh</h3>
+          }
+        >
+          <div className="mt-3">
+            {feedsData.map(
+              ({ feedId, picture, content, createdOn, feedMetaData, user }) => (
+                <FeedCard
+                  key={feedId}
+                  feedId={feedId}
+                  picture={picture}
+                  content={content}
+                  createdOn={createdOn}
+                  username={user.username}
+                  firstName={user.firstName}
+                  lastName={user.lastName}
+                  profilePicture={user.profile ? user.profile.picture : null}
+                  feedMetaData={feedMetaData}
+                />
+              )
+            )}
+          </div>
+        </InfiniteScroll>
+      </article>
     </main>
   );
 };
--- src/util/ApiUtil.jsx
+++ src/util/ApiUtil.jsx
@@ -233,3 +233,23 @@ export const addFeedMetaDataApi = async (token, feedId, isLike, comment) => {
     return response;
   }
 };
+
+export const getOthersFeedsApi = async (token, pageNumber) => {
+  let response = frameResponse();
+  try {
+    const url = `${API_BASE_URL}/feeds/other/${pageNumber}/5`;
+    const apiResponse = await axios.get(url, {
+      headers: { Authorization: frameToken(token) },
+    });
+    if (apiResponse.status === 200) {
+      response = frameResponse(1, apiResponse.data);
+    }
+  } catch (err) {
+    if (err.response) {
+      response = frameResponse(0, err.response.data.message);
+    }
+    console.log(err);
+  } finally {
+    return response;
+  }
+};
