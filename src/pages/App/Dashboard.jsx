79e4be6466f59fbff884b0157976cae8faba87a0
jeffbptn <jeff@bptn.com>
2025-05-22 09:22:58 -0400

    Coding 11.33

--- src/pages/App/Dashboard.jsx
+++ src/pages/App/Dashboard.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import MyProfile from "../../components/MyProfile";
+import AddFeed from "../../components/AddFeed";
 
 const Dashboard = () => {
-  return <MyProfile />;
+  return (
+    <main>
+      <MyProfile />
+      <AddFeed />
+    </main>
+  );
 };
 
 export default Dashboard;
